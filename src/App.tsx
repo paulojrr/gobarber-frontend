483ec43a6ce94ee327f5a806e1746d8fe11466cd
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2021-03-31 20:37:10 -0300

    Habilitando CORS na API, API de Contexto

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 import SignIn from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Paulo' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,12 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
 import getValidationErrors from '../../utils/getValidationErrors';
+
+import AuthContext from '../../context/AuthContext';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -15,6 +17,9 @@ import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const { name } = useContext(AuthContext);
+  console.log(name);
 
   const handleSubmit = useCallback(async (data: Record<string, unknown>) => {
     try {
